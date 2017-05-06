1ff6e5954affb0bdc0e8789a68bde858aa2a787f
野田実 <n0dam1@nodaminorunoMacBook-Air.local>
2017-05-07 00:13:59 +0900

    dive02相当

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,6 @@
 class PicturesController < ApplicationController
+  before_action :set_picture, only: [:edit, :update, :destroy]
+
   def index
     @pictures = Picture.all
   end
@@ -8,12 +10,36 @@ class PicturesController < ApplicationController
   end
 
   def create
-    Picture.create(pictures_params)
-    redirect_to pictures_path
+    @picture = Picture.new(pictures_params)
+    if @picture.save
+      # 一覧画面へ遷移して"写真を投稿しました！"とメッセージを表示します。
+      redirect_to pictures_path, notice: "写真を投稿しました！"
+    else
+      # 入力フォームを再描画します。
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @picture.update(pictures_params)
+    redirect_to pictures_path, notice: "写真を編集しました！"
+  end
+
+  def destroy
+    @picture.destroy
+    redirect_to pictures_path, notice: "写真を削除しました！"
   end
 
   private
     def pictures_params
       params.require(:picture).permit(:title, :content)
+    end
+
+    # idをキーとして値を取得するメソッド
+    def set_picture
+      @picture = Picture.find(params[:id])
     end
 end
