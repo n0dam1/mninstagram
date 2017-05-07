18140b84ea9ecc6aff6547ad36cf7fe897f508aa
minoru_noda <minoru.noda.0527@gmail.com>
2017-05-07 20:50:29 +0900

    exam02

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507102039) do
+ActiveRecord::Schema.define(version: 20170507114520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "pictures", force: :cascade do |t|
-    t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
